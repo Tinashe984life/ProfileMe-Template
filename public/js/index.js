e1163b57e3551216d01c7cf0ae2a426222b05eb8
Tinashe Madanire <madtinashe@gmail.com>
2022-09-08 01:14:36 +0200

    Added a motivation info tab + whatsapp & sms links

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 const dd = document.getElementById('dd');
 const dd2 = document.getElementById('dd2');
 const dd3 = document.getElementById('dd3');
+const dd4 = document.getElementById('dd4');
 const amc = document.getElementById('amc');
 const wec = document.getElementById('wec');
 const pc = document.getElementById('pc');
+const mc = document.getElementById('mc');
 
 dd.addEventListener('click', () => {
     amc.classList.toggle('amc');
@@ -15,4 +17,8 @@ dd2.addEventListener('click', () => {
 
 dd3.addEventListener('click', () => {
     pc.classList.toggle('amc');
+})
+
+dd4.addEventListener('click', () => {
+    mc.classList.toggle('amc');
 })
